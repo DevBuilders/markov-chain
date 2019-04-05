0f8526707ba83fdd7ac8f6d61606bba77bc07470
Brian Bolte <brian.bolte.contractor@8451.com>
2019-04-05 17:27:04 -0400

    Convert to bigrams (in-progress)

--- markhov.ts
+++ markhov.ts
@@ -2,42 +2,63 @@ const fs = require('fs');
 const { sentences } = require('sbd');
 let wordArray: string[];
 let inputText: string;
-const str: string = fs.readFileSync("input.txt", { encoding: "utf-8"});
+const mobyDick: string = fs.readFileSync("input.txt", { encoding: "utf-8"});
+
+// Moby Dick
+console.log(mobyDick.slice(0,100))
 
 const START = Symbol("START");
 const END = Symbol("END");
-const strSentences = sentences(str).map(s => s.split(/\s+|(?=[?!.]$)/))
+console.log(sentences(mobyDick));
+const strSentences: string[][] = sentences(mobyDick)
+  .map(s => s.split(/\s+|(?=[?!.]$)/))
   .map(sArr => [START, ...sArr.filter(s => s), END]);
 wordArray = [].concat.apply([], strSentences);
 
+type FavoriteThing = 'puppies' | 'rainbows' | 'typescript';
+
+let foo: FavoriteThing;
+
 console.log(strSentences);
 console.log(wordArray);
-interface Acc {
+interface Accumulator {
   chain: {
-    [START]?: (string | Symbol)[]
-    [word: string]: (string | Symbol)[]
+    [START]?: {
+      [bigramWord: string]: (string | typeof END)[];
+    }
+    [word: string]: {
+      [bigramWord: string]: (string | typeof END)[];
+    }
   };
-  lookback: string;
+  lookback: [string, string];
 }
-const markhovChain = wordArray.reduce<Acc>(
-  (acc: Acc, word: string) => {
+
+const markhovChain = wordArray.reduce<Accumulator>(
+
+  (acc: Accumulator, word: string) => {
+    const [x, y] = acc.lookback; 
+
     if (acc.lookback) {
-      acc.chain[acc.lookback].push(word);
+      acc.chain[x][y].push(word);
     }
     if (!acc.chain[word]) {
-      acc.chain[word] = [];
+      acc.chain[word] = {};
     }
-    acc.lookback = word;
+    if(!acc.chain[y][word]) {
+      acc.chain[y][word] = [];
+    }
+
+    acc.lookback = [acc.lookback[1], word];
     return acc;
   },
-  { chain: {}, lookback: null }
+  { chain: {}, lookback: [null, null] }
 ).chain;
 
-const pickRandom = <T>(arr: T[]) => arr[Math.floor(Math.random() * arr.length)];
+const pickRandom = <T>(arr: T[]): T => arr[Math.floor(Math.random() * arr.length)];
 
-function markhov(word: string | Symbol, maxLength: number = 500): string {
+function markhov(word: string | typeof START | typeof END = START, maxLength: number = 500): string {
   if (word === END || maxLength < 0) return '';
-  const nexts = markhovChain[word as string];
+  const nexts = markhovChain[word];
   if (!nexts) 
     throw new Error("Initial word not in corpus");
   let nextWord = pickRandom(nexts);
@@ -45,4 +66,8 @@ function markhov(word: string | Symbol, maxLength: number = 500): string {
   return (word !== START ? word : '') + (['!','?','.'].includes(rest[0]) ? '' : " ") + rest;
 }
 
-console.log(markhov(START));
+console.log(markhov());
+console.log(markhov());
+console.log(markhov());
+console.log(markhov("Call"));
+
